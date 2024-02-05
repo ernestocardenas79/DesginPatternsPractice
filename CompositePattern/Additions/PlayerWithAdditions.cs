4d8fcd85ef31fca5aa2e14ead53ca04c9da6f0e3
Ernesto Cardenas <ernesto.cardenas79@gmail.com>
2024-02-05 07:11:05 -0700

    Shield | name added

--- CompositePattern/Additions/PlayerWithAdditions.cs
+++ CompositePattern/Additions/PlayerWithAdditions.cs
@@ -7,11 +7,15 @@ public abstract class ShieldProtection
 {
     protected ShieldProtection Shield { get; init; }
     protected int Protection {get; init;}
-    public ShieldProtection()
-    { }
-    public ShieldProtection(ShieldProtection shield)
+    private readonly string Name;
+    public ShieldProtection(string name)
+    {
+        Name=name;
+     }
+    public ShieldProtection(string name, ShieldProtection shield)
     {
         Shield = shield;
+        Name=name;
     }
     internal virtual double CalculateProtection(double damage)
     {
@@ -21,13 +25,13 @@ public abstract class ShieldProtection
 
     protected void PrintProtection(double damage)
     {
-        Console.WriteLine($"Protection: {Protection}, Input Damage: {damage} Total Damage: {damage - Protection}\n");
+        Console.WriteLine($"{Name} Protection: {Protection}, Input Damage: {damage} Total Damage: {damage - Protection}\n");
     }
 }
 
 public class NoShield : ShieldProtection
 {
-    public NoShield()
+    public NoShield():base("NoShield")
     {
     }
     internal sealed override double CalculateProtection(double damage)
@@ -38,7 +42,7 @@ public class NoShield : ShieldProtection
 
 public class MeshTshirt : ShieldProtection
 {
-    public MeshTshirt(ShieldProtection shield) : base(shield)
+    public MeshTshirt(ShieldProtection shield) : base("Mesh",shield)
     { 
         Protection =1;
     }
@@ -52,7 +56,7 @@ public class MeshTshirt : ShieldProtection
 
 public class Vest : ShieldProtection
 {
-    public Vest(ShieldProtection shield) : base(shield)
+    public Vest(ShieldProtection shield) : base("Vest",shield)
     {
         Protection =3;
     }
